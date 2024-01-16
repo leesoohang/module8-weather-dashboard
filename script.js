c34498bfdced09b85af3ab1f4f3498a22e8681b8
Soo <li_xiuheng@hotmail.com>
2024-01-16 01:17:00 +0000

    completed local storage setup

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-// click event when weather info shows up
 
+var cities = [];
+// get curernt weather and forecast
+function getWeather(city) {
 
-$("#search-button").click( function(event) {
-    event.preventDefault();
-var city = $("#search-input").val()
 var current = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=fd62aede53b324434c4bed0886f183e0&q=" + city
 
-
-// click event when buttons (btn btn-secondary) generated in #history div
-var history = $("<button>");
-history.addClass("btn btn-secondary mb-2");
-
-
-if (city) {
-    $("#history").append(history);
-} else {
-    alert("Please input!")
-}
 
 // retrieve data from Current Weather API
 fetch(current).then( function(response) {
@@ -27,16 +15,22 @@ fetch(current).then( function(response) {
     var temp = today.main.temp;
     var wind = today.wind.speed;
     var humidity = today.main.humidity;
-    var icon = today.weather[0].icon
-    history.text(city);    
-    console.log(city + "\n" + date + "\nTemperature: " + temp + "℃" + "\nWind speed: " + wind + "kph" + "\nHumidity: " + humidity + "%");
+    var icon = today.weather[0].icon;
+// stop adding existing cities to history
+    var prevSearch = cities.includes(city);
+    if (!prevSearch) {
+        cities.push(city)
+        cityHistory();
+        addHistory(city);
+    }
+
     $("#today").empty();
-    
+   
 // create current weather component
     $("#today").append("<div class='card' id='current'>");
     $("#current").append("<div class='card-body'><div class='card-text' id='current-item'>");
     
-    $("#current-item").append("<h2 class='fw-bold'>" + city + " (" + date + ") " + "<img src='https://openweathermap.org/img/wn/" + icon + ".png'>")
+    $("#current-item").append("<h3 class='fw-bold' value='"+ city+ "'>" + city + " (" + date + ") " + "<img src='https://openweathermap.org/img/wn/" + icon + ".png'>")
     $("#current-item").append("<p> Temp: " + temp + "℃" + 
     "<p> Wind: " + wind + " kph" + 
     "<p> Humidity: " + humidity + " %");
@@ -60,7 +54,7 @@ fetch(forecast).then( function(response) {
     var wind = future[timeDay[i]].wind.speed;
     var humidity = future[timeDay[i]].main.humidity;
     var icon = future[timeDay[i]].weather[0].icon;
-    console.log(future[timeDay[i]]);
+
 // empty cards so there won't be duplicate results 
  
 // create forecast card group component
@@ -73,18 +67,52 @@ fetch(forecast).then( function(response) {
     "<p class='card-text'> Wind: " + wind + " kph" + 
     "<p class='card-text'> Humidity: " + humidity + " %");
 }
+})
      
-   }
+}
+
+$("#search-button").click( function(event) {
+    event.preventDefault();
+    var cityInput = $("#search-input").val()
+    getWeather(cityInput);
+    }
 )
 
-$("#history").append("<input class='btn btn-light' id='clear' type='reset' value='Clear history'>")
 
-// clear history event
-$("#clear").click( function(){
-    $("#history").empty()}
-);
-})
+// local storage
+
+function loadCities() {
+    var citiesLoaded = localStorage.getItem("cities")
+    if(!citiesLoaded) {
+        return false;
+    }
+    
+    citiesLoaded = JSON.parse(citiesLoaded);
+    
+    for (j = 0; j < citiesLoaded.length; j++) {
+        addHistory(citiesLoaded[j])
+        cities.push(citiesLoaded[j])
+    }
+}
+
+function cityHistory() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
 
 
+function addHistory(city) {
+    var history = $("<button>");
+    
+    history.addClass("btn btn-secondary mb-2 city");
+    history.attr("value", city)
+    history.text(city);
+    $(".city").click( function() {
+        let cityInput = $(this).attr("value")
+        getWeather(cityInput);
+    });      
+    $("#history").append(history);
+    }
 
+  
 
+loadCities();
